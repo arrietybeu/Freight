93ad328bb8fde8a08276655a9484c7d12a7209f7
arrietybeu <arrietybeu@gmail.com>
2026-02-16 03:55:52 +0700

    style: Restore .gitignore and remove unnecessary build artifacts

--- server/target/debug/deps/Freight-76c01c0eb971603a.d
+++
@@ -1,5 +0,0 @@
-/Users/arriety/RustroverProjects/Freight/target/debug/deps/Freight-76c01c0eb971603a.d: src/main.rs
-
-/Users/arriety/RustroverProjects/Freight/target/debug/deps/libFreight-76c01c0eb971603a.rmeta: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/Freight-991f34a218cbdac8.d
+++
@@ -1,5 +0,0 @@
-/Users/arriety/RustroverProjects/Freight/target/debug/deps/Freight-991f34a218cbdac8.d: src/main.rs
-
-/Users/arriety/RustroverProjects/Freight/target/debug/deps/libFreight-991f34a218cbdac8.rmeta: src/main.rs
-
-src/main.rs:
