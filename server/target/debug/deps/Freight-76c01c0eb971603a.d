aced90f3e900a9eee6dd4d43e0671220c264619e
arrietybeu <arrietybeu@gmail.com>
2026-02-16 03:54:44 +0700

    style: Add incremental build artifacts for Freight project
    - Created query-cache.bin with version information and binary data.
    - Added work-products.bin to store incremental build products.
    - Initialized lock file for incremental build tracking.

---
+++ server/target/debug/deps/Freight-76c01c0eb971603a.d
@@ -0,0 +1,5 @@
+/Users/arriety/RustroverProjects/Freight/target/debug/deps/Freight-76c01c0eb971603a.d: src/main.rs
+
+/Users/arriety/RustroverProjects/Freight/target/debug/deps/libFreight-76c01c0eb971603a.rmeta: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/Freight-991f34a218cbdac8.d
@@ -0,0 +1,5 @@
+/Users/arriety/RustroverProjects/Freight/target/debug/deps/Freight-991f34a218cbdac8.d: src/main.rs
+
+/Users/arriety/RustroverProjects/Freight/target/debug/deps/libFreight-991f34a218cbdac8.rmeta: src/main.rs
+
+src/main.rs:
